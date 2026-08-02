08ae58911cc1f5bebf935808173425a2138df047
vishwakbalaji-a <vishwakbalaji2304@gmail.com>
2026-08-02 18:13:24 +0530

    created Dynakic url using url tag

--- Blog/urls.py
+++ Blog/urls.py
@@ -1,6 +1,8 @@
 from . import views
 from django.urls import path
 
+app_name ='blog'
+
 urlpatterns =[
     path("index/",views.index,name="index"),
     path('post/<str:post_id>/',views.detail,name="detail"),
--- Blog/views.py
+++ Blog/views.py
@@ -5,10 +5,10 @@ from django.http import HttpResponse
 def index(request):
     blog_title = 'Latest posts'
     posts = [
-        {"title": "Post 1", "content": "Content of post 1"},
-        {"title": "Post 2", "content": "Content of post 2"},
-        {"title": "Post 3", "content": "Content of post 3"},
-        {"title": "Post 4", "content": "Content of post 4"},
+        {"id": 1, "title": "Post 1", "content": "Content of post 1"},
+        {"id": 2,"title": "Post 2", "content": "Content of post 2"},
+        {"id": 3,"title": "Post 3", "content": "Content of post 3"},
+        {"id": 4,"title": "Post 4", "content": "Content of post 4"},
     ]
     return render(request,'blog/index.html',{'blog_title': blog_title , 'posts':posts})
 def detail(request,post_id):
