cd4f98d91c22e807476fe04029868c9a95a3d483
vishwakbalaji-a <vishwakbalaji2304@gmail.com>
2026-08-02 18:41:17 +0530

    Created custom 404 error page

--- Main/settings.py
+++ Main/settings.py
@@ -23,9 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7a2-lo%ok9tvoiszb&tk7q!awb9_+a#*f9ds)yu%n+e&(jr5gk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1'
+]
 
 
 # Application definition
@@ -55,7 +58,7 @@ ROOT_URLCONF = 'Main.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- Main/urls.py
+++ Main/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import handler404
 from django.contrib import admin
 from django.urls import path , include
+
+handler404 = "Main.views.custom_page_not_found"
 
 urlpatterns = [
     path('blog/',include("Blog.urls")),
