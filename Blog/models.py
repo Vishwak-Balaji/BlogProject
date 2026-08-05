53c2bcc85701dc7f57175459fc190fc2495b9468
vishwakbalaji-a <vishwakbalaji2304@gmail.com>
2026-08-05 15:52:43 +0530

    inserting data

--- Blog/models.py
+++ Blog/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Post(models.Model):
-    title = models.CharField(max_length=100)
-    content = models.TextField()
-    img_url = models.URLField(null=True)
+    titles = models.CharField(max_length=100)
+    contents = models.TextField()
+    img_urls = models.URLField(null=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.title
+        return self.titles
